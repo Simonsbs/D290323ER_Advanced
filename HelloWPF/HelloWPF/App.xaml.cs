ef2a784fa8e4e76294e35e36e09966f2a025eaeb
Simon B.Stirling <simonsbs@gmail.com>
2024-03-13 18:02:13 +0200

    Created WPF Hello world project

---
+++ HelloWPF/HelloWPF/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace HelloWPF;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application {
+}
+
