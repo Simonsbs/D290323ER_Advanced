bd7d1713865fa6cf1484b907b0dfdb3666dcd442
Simon B.Stirling <simonsbs@gmail.com>
2024-03-20 18:08:22 +0200

    .

---
+++ HelloWPF/DelegatesAndEvents/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace DelegatesAndEvents;
+
+internal class Program {
+	public delegate void MyDelegate(string message);
+
+	static void Main(string[] args) {
+		MyDelegate del = new MyDelegate(MyFunction);
+
+		//del("Simon");
+
+		DoSomeWork(new MyDelegate(MyFunction));
+		
+		DoSomeWork(new MyDelegate(MyFunction2));
+	}
+
+	public static void DoSomeWork(MyDelegate logger) {
+		// DO STUFF
+		logger("log...");
+		// do stuff
+		logger("More log...");
+	}
+
+	public static void MyFunction2(string text) {
+		File.AppendAllText("log.txt", text + Environment.NewLine);
+	}
+
+	public static void MyFunction(string text) {
+		Console.WriteLine(text);
+	}
+}
