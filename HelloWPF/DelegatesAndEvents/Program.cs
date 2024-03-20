c1a5d561d27ad8c98721a3e9d50c26c8dbb540ed
Simon B.Stirling <simonsbs@gmail.com>
2024-03-20 20:44:20 +0200

    .

--- HelloWPF/DelegatesAndEvents/Program.cs
+++ HelloWPF/DelegatesAndEvents/Program.cs
@@ -55,7 +55,36 @@ internal class Program {
 		 *
 		 */
 
-
+		/*
+		 *
+		 * Step 1: create a delegate called "TimerNotification"
+		 * returns void and takes an integer
+		 *
+		 * Step 2: Create a Timer class
+		 * create 2 events using the delegate above:
+		 * TimerTick
+		 * TimerCompleted
+		 *
+		 * Step 3:
+		 * Add a function to the Timer class called "Start" that
+		 * takes an integer (time to run in seconds)
+		 * the function needs to loop the amount given in the parameter
+		 * each iteration sleep for 1000 milli seconds and then raise
+		 * the TimerTick event.
+		 * When the loop completes, call the TimerCompleted event.
+		 *
+		 * Step 4:
+		 * in the Main
+		 * create an instance of Timer
+		 * register for each of the events with a function suited
+		 * call the "Start" on the timer and run the code
+		 *
+		 * verify that you get the tick event notifications
+		 * and the completed
+		 *
+		 *
+		 *
+		 */
 		
 	}
 
