877c7455820cfc4715f619e5eaf1a6d524533e29
Simon B.Stirling <simonsbs@gmail.com>
2024-03-31 18:25:57 +0300

    .

---
+++ HelloWPF/AccessModifiers2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Drawing;
+
+namespace AccessModifiers2;
+
+internal class Program {
+	static void Main(string[] args) {
+		Cat c = new Cat();
+		// unable to access due to protection level
+		//c.MakeSound();
+		//c.MakeAnotherSound();
+		//c.isAlive = true;
+		//c.Color = "Green";
+
+
+		Animal a = new Animal();
+		// unable to access due to protection level
+		//a.MakeSound();
+		//a.MakeAnotherSound();
+		//a.isAlive = true;
+		//a.Color = "Green";
+
+		Dog d = new Dog();
+	}
+
+}
+
+/*
+public class Animal {
+	protected bool isAlive;
+
+	protected string Color { get; set; }
+
+	protected virtual void MakeSound() {
+		Console.WriteLine("Generic animal sound");
+		MakeAnotherSound();
+	}
+
+	protected void MakeAnotherSound() {
+		Console.WriteLine("More sounds");
+	}
+}
+
+public class Cat : Animal {
+
+	protected override void MakeSound() {
+		Console.WriteLine("Meow");
+
+		MakeAnotherSound();
+
+		isAlive = true;
+		Color = "Green";
+	}
+}
+
+*/
---
+++ HelloWPF/MyLibrary/Animal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary {
+	public class Animal {
+		public string Name { get; set; }
+		public string Description { get; set; }
+
+		public virtual void MakeSound() {
+			Console.WriteLine("Generic animal sound");
+		}
+	}
+
+	public class Cat : Animal {
+		public Cat(string name) {
+			Name = name;
+		}
+
+		public override void MakeSound() {
+			Console.WriteLine("Meow");
+		}
+	}
+
+	public class Dog : Animal {
+		public Dog(string name) {
+			Name = name;
+		}
+
+		public override void MakeSound() {
+			Console.WriteLine("Woof!");
+		}
+	}
+}
