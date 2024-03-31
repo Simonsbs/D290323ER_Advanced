0ecb9fcb14d9b274dbd80b738d12db30479e2d9c
Simon B.Stirling <simonsbs@gmail.com>
2024-03-31 20:58:07 +0300

    .

---
+++ HelloWPF/GenericsDemo/ConsoleLogger.cs
@@ -0,0 +1,7 @@
+﻿namespace GenericsDemo;
+
+public class ConsoleLogger<T> {
+	public void WriteToLog(T message) {
+		Console.WriteLine($"Message: {message} ; Type: {typeof(T)}");
+	}
+}
---
+++ HelloWPF/GenericsDemo/MyStack.cs
@@ -0,0 +1,25 @@
+﻿namespace GenericsDemo;
+
+public class MyStack<T> {
+	public readonly T[] _items;
+	private int _currentIndex = -1;
+
+	public MyStack() => _items = new T[10];
+
+	public int Count => _currentIndex + 1;
+
+	public void Push(T item) {
+		_items[++_currentIndex] = item;
+	}
+
+	public T Pop() {
+		return _items[_currentIndex--];
+	}
+
+	/*
+	public double Pop() {
+		return _items[_currentIndex--];
+	}
+	*/
+
+}
