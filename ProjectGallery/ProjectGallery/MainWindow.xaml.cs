c24f4fc20a793fa23abf21d790b94bcbb4245711
Simon B.Stirling <simonsbs@gmail.com>
2024-04-03 18:31:52 +0300

    init

---
+++ ProjectGallery/ProjectGallery/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ProjectGallery.Controls;
+
+namespace ProjectGallery;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window {
+	private object[] projects = new[] {
+		new object(),
+		new object(),
+		new object(),
+		new object()
+	};
+
+	public MainWindow() {
+		InitializeComponent();
+		InitializeProjectButtons();
+	}
+
+	private void InitializeProjectButtons() {
+		foreach (var project in projects) {
+			//Button button = new Button() {
+			//	Width = 100,
+			//	Height = 100,
+			//	Margin = new Thickness(10)
+			//};
+
+			//StackPanel pnl = new StackPanel();
+
+			//pnl.Orientation = Orientation.Vertical;
+
+			//Image img = new Image();
+			//img.Width = 50;
+			//img.Height = 50;
+
+			//TextBlock tb = new TextBlock();
+			//tb.Text = "Button";
+			//tb.VerticalAlignment = VerticalAlignment.Center;
+
+			//pnl.Children.Add(img);
+			//pnl.Children.Add(tb);
+			
+			//button.Content = pnl;
+
+			ProjectButton button = new ProjectButton {
+				Margin = new Thickness(10),
+				Width = 100,
+				Height = 130
+			};
+			ProjectsPanel.Children.Add(button);
+		}
+	}
+}
