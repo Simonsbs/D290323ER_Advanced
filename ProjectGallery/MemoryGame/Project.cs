ee2c9ae28ec8fa183c93e2f691ae2c6e55cb5d51
Simon B.Stirling <simonsbs@gmail.com>
2024-04-07 17:57:04 +0300

    .

--- ProjectGallery/MemoryGame/Project.cs
+++ ProjectGallery/MemoryGame/Project.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media.Imaging;
@@ -10,7 +11,16 @@ namespace MemoryGame;
 public class Project : IProjectMeta {
 	public string Name { get; set; } = "Memory Game";
 
-	public BitmapImage Icon => new BitmapImage(new Uri($"{AppDomain.CurrentDomain.BaseDirectory}/Resources/Plus.png", UriKind.Absolute));
+	public BitmapImage Icon {
+		get {
+			//return new BitmapImage(new Uri($"{AppDomain.CurrentDomain.BaseDirectory}/Resources/Main.png", UriKind.Absolute));
+
+			string assemblyName = Assembly.GetExecutingAssembly().GetName().Name;
+			Uri uri = new Uri($"pack://application:,,,/{assemblyName};component/Resources/Plus.png");
+
+			return new BitmapImage(uri);
+		}
+	}
 
 	public void Run() {
 		MainWindow window = new MainWindow();
