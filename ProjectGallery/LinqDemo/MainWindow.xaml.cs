aa790dc435191d89643e519a33c8fa92156be9d9
Simon B.Stirling <simonsbs@gmail.com>
2024-05-01 18:28:15 +0300

    .

--- ProjectGallery/LinqDemo/MainWindow.xaml.cs
+++ ProjectGallery/LinqDemo/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,11 +12,20 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace LinqDemo;
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window {
+	List<Product> products;
+
 	public MainWindow() {
 		InitializeComponent();
+
+		LoadProducts();
+		
+		//AddButtons();
+	}
+
+	private void LoadProducts() {
+		string rawJson = File.ReadAllText("Products.json");
+		products = JsonSerializer.Deserialize<List<Product>>(rawJson);
+		ResultsDataGrid.ItemsSource = products;
 	}
 }
