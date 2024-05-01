e988e59905ca74a21a9031dea5d84789d6eec05f
Simon B.Stirling <simonsbs@gmail.com>
2024-05-01 18:13:11 +0300

    .

---
+++ ProjectGallery/LinqDemo/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace LinqDemo;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application {
+}
+
