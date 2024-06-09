7f8f0d39aa5b5b0cec1516ce8df6ee7b2196763d
Simon B.Stirling <simonsbs@gmail.com>
2024-06-09 17:55:04 +0300

    Json Demo

---
+++ JsonReadWriteDemo/JsonReadWriteDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+namespace JsonReadWriteDemo;
+
+internal class Program {
+	static void Main(string[] args) {
+		// Read from file
+		string rawData = File.ReadAllText("People.json");
+		var people = JsonSerializer.Deserialize<List<Person>>(rawData);
+
+		// Write to file
+		List<Person> newPeople = new() {
+			new Person { Name = "Alice", Age = 25 },
+			new Person { Name = "Bob", Age = 30 }
+		};
+		string json = JsonSerializer.Serialize(newPeople, new JsonSerializerOptions {
+			WriteIndented = true 
+		});
+		File.WriteAllText("NewPeople.json", json);
+	}
+}
+
+internal class Person {
+	public string Name { get; set; }
+	public int Age { get; set; }
+}
